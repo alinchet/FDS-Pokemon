6465f56848abe454bc34b176c6672658f8dc8433
A <a@MBP-de-A.station>
2025-11-05 14:51:18 +0100

    small typo fixed

--- feature_extraction.py
+++ feature_extraction.py
@@ -158,7 +158,7 @@ def extract_full_features(data: list[dict], progress_bar=None) -> pd.DataFrame:
 
         battle_features['p1_residual_hp_pct'] = p1_residual_hp_pct
         battle_features['p2_residual_hp_pct'] = p2_residual_hp_pct
-        battle_features['p1_players_with_low_hp_pct'] = p2_players_with_low_hp_pct
+        battle_features['p1_players_with_low_hp_pct'] = p1_players_with_low_hp_pct
         battle_features['p2_players_with_low_hp_pct'] = p2_players_with_low_hp_pct        
         battle_features['hp_pct_advantage'] = int(p1_residual_hp_pct > p2_residual_hp_pct)
             
